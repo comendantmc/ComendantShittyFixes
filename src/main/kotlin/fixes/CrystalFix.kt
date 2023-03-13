1b43e53c03e294d5727670d3ca00559e2c3d790b
VVriter <romannovohat@gmail.com>
2023-03-13 18:34:22 +0200

    Optional Silent Switch Fix, i think it will work, just test.
    im too lazy to download server code xd -_-

--- src/main/kotlin/fixes/CrystalFix.kt
+++ src/main/kotlin/fixes/CrystalFix.kt
@@ -1,9 +1,12 @@
 package fixes
 
+import org.bukkit.Material
 import org.bukkit.entity.EnderCrystal
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.entity.EntityDamageByEntityEvent
+import org.bukkit.event.player.PlayerInteractEvent
+import org.bukkit.inventory.ItemStack
 import org.bukkit.plugin.Plugin
 
 
@@ -15,6 +18,22 @@ class CrystalFix(val plugin: Plugin) {
             && event.entity.ticksLived <= plugin.config.getInt("crystal.minLived")
         ) {
             event.isCancelled = true
+        }
+    }
+
+    @EventHandler
+    private fun onPlayerInteractEvent(event: PlayerInteractEvent) {
+        if (!plugin.config.getBoolean("silentSwitchFix")) return
+        val player = event.player
+        val inventory = player.inventory
+        val mainHandItem: ItemStack = inventory.itemInMainHand
+        val offHandItem: ItemStack = inventory.itemInOffHand
+
+        if (event.action.name == "RIGHT_CLICK_BLOCK" || (event.clickedBlock.type == Material.BEDROCK || event.clickedBlock.type == Material.OBSIDIAN)) {
+            if (!((mainHandItem.type == Material.END_CRYSTAL) ||
+                (offHandItem.type == Material.END_CRYSTAL))) {
+                event.isCancelled = true
+            }
         }
     }
 }
